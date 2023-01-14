1942c72ff000043a7c21d1dc5019ec6220e66613
Victor Philip <5893811+vphilipnyc@users.noreply.github.com>
2023-01-14 11:34:19 -0500

    make this functional to be nice to the compiler (#27)

--- src/main/java/com/vphilipnyc/decision/Persistable.java
+++ src/main/java/com/vphilipnyc/decision/Persistable.java
@@ -3,6 +3,7 @@ package com.vphilipnyc.decision;
 /**
  * Contract to fulfill an ID requirement for an item.
  */
+@FunctionalInterface
 public interface Persistable {
     Object getId(); //Some DBs need this to be a String, but some lean toward Longs
 }
